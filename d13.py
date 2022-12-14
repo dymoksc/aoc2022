fdb54539520a295c264b43e80404120938afd68a
Gleb Petrovichev <gleb.petrovichev@travelportgds.cz>
2022-12-14 07:56:59 +0100

    Day 13 - Part 2

--- d13.py
+++ d13.py
@@ -1,29 +1,30 @@
 import fileinput
 import json 
+import functools
 from pprint import pprint
 from typing import List, Union
 
 def compare (lhs: Union[List, int], rhs: Union[List, int]) -> int:
-	print("Comparing {} and {}".format(lhs, rhs))
+	# print("Comparing {} and {}".format(lhs, rhs))
 
 	if isinstance(lhs, int) and isinstance(rhs, int):
-		print("Both are ints, returning difference")
+		# print("Both are ints, returning difference")
 		return lhs - rhs
 
 	if isinstance(lhs, list) and isinstance(rhs, int):
-		print("Wrapping rhs in list")
+		# print("Wrapping rhs in list")
 		return compare(lhs, [rhs])
 	if isinstance(rhs, list) and isinstance(lhs, int):
-		print("Wrapping lhs in list")
+		# print("Wrapping lhs in list")
 		return compare([lhs], rhs)
 
 	for i in range(min(len(lhs), len(rhs))):
-		print("Comparing", i, "element")
+		# print("Comparing", i, "element")
 		v = compare(lhs[i], rhs[i])
 		if v != 0:
 			return v
 
-	print("Returning len difference")
+	# print("Returning len difference")
 	return len(lhs) - len(rhs)
 
 i = 0
@@ -32,17 +33,25 @@ s = 0
 pairNum = 1
 for line in fileinput.input():
 	line = line.rstrip()
-	result = 0
-	if i % 3 == 0:
+	if line != "":
 		pair.append(json.loads(line))
-	elif i % 3 == 1:
-		pair.append(json.loads(line))
-		if compare(pair[0], pair[1]) < 0:
-			s = s + pairNum
-	else:
-		print("==============")
-		pair = []
-		pairNum = pairNum + 1
-	i = i + 1
+	# result = 0
+	# if i % 3 == 0:
+	# 	pair.append(json.loads(line))
+	# elif i % 3 == 1:
+	# 	pair.append(json.loads(line))
+	# 	if compare(pair[0], pair[1]) < 0:
+	# 		s = s + pairNum
+	# else:
+	# 	# print("==============")
+	# 	pair = []
+	# 	pairNum = pairNum + 1
+	# i = i + 1
 
-print("sum: ", s)
+# # print("sum: ", s)
+pair.append([[2]])
+pair.append([[6]])
+
+for i, x in enumerate(sorted(pair, key=functools.cmp_to_key(compare))):
+	if x == [[2]] or x == [[6]]:
+		pprint("{}: {}".format(i + 1, x))
