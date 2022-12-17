1a6f5a3f3d584ba6383cdd35736ba6b924936c65
Gleb Petrovichev <gpetrovichev@purestorage.com>
2022-12-17 12:43:34 +0100

    Day 15 - part 2

--- d15.py
+++ d15.py
@@ -47,11 +47,15 @@ def merge_intervals(intervals: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
 assert(merge_intervals([(0, 1), (9, 12)]) == [(0, 1), (9, 12)])
 assert(merge_intervals([(0, 2), (-1, 2), (9, 12)]) == [(-1, 2), (9, 12)])
 
-required_y = 2000000
-sharp_intervals = []
+required_y = 10
+# max_coord = 20  # TODO: change to 4000000
+max_coord = 4000000  # TODO: change to 4000000
+
+sharp_intervals = [[] for i in range(max_coord + 1)]
 sensors_and_beacons_on_line = set()
 
 # Parsing
+print("Parsing...")
 rx = re.compile(r"Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)")
 beacons_by_sensors = {}
 # maxX = 0
@@ -68,13 +72,17 @@ for line in fileinput.input():
 	# maxY = max(maxY, int(m[2]) + distance)
 	# minY = min(minY, int(m[2]) - distance)
 
-	diff = abs(required_y - int(m[2]))
-	if diff <= distance:
-		sharp_intervals.append((int(m[1]) - (distance - diff), int(m[1]) + (distance - diff)))
-	if int(m[2]) == required_y:
-		sensors_and_beacons_on_line.add(int(m[2]))
-	if int(m[4]) == required_y:
-		sensors_and_beacons_on_line.add(int(m[4]))
+	for test_y in range(max_coord + 1):
+		if test_y % 100000 == 0:
+			print(test_y / max_coord * 100)
+		diff = abs(test_y - int(m[2]))
+		if diff <= distance:
+			sharp_intervals[test_y].append((max(0, int(m[1]) - (distance - diff)),
+											min(max_coord, int(m[1]) + (distance - diff)) + 1))
+		# if int(m[2]) == required_y:
+		# 	sensors_and_beacons_on_line.add(int(m[2]))
+		# if int(m[4]) == required_y:
+		# 	sensors_and_beacons_on_line.add(int(m[4]))
 
 # Building field
 # field = [['.'] * (maxX - minX + 1) for i in range(maxY - minY + 1)]
@@ -89,6 +97,13 @@ for line in fileinput.input():
 # 			x = s[0] + j
 # 			if field[y][x] == '.':
 # 				field[y][x] = '#'
+#
+# for i in range(0, max_coord + 1):
+# 	for j in range(0, max_coord + 1):
+# 		field[i][j] = '_' if field[i][j] != '.' else '@'
+#
+# print(field[11][14])
+# field[11][14] = '='
 
 # Plotting field
 # print('    ', end='')
@@ -106,8 +121,21 @@ for line in fileinput.input():
 
 # print(sum(x[1] - x[0] for x in sharp_intervals))
 
-merged_intervals = merge_intervals(sharp_intervals)
-print(sharp_intervals)
-print(merged_intervals)
-print(sensors_and_beacons_on_line)
-print(sum(x[1] - x[0] + 1 for x in merged_intervals) - len(sensors_and_beacons_on_line))
+print("Searching...")
+for y, sharp_interval in enumerate(sharp_intervals):
+	merged_interval = merge_intervals(sharp_interval)
+	if len(merged_interval) == 1 and merged_interval[0][0] > 0:
+		print((0, y))
+	elif len(merged_interval) == 1 and merged_interval[0][1] < max_coord + 1:
+		print((max_coord, y))
+	elif len(merged_interval) == 2:
+		print((merged_interval[0][1], y))
+
+
+# 12|3||4567891|234|56789
+
+# merged_intervals = [merge_intervals(sharp_interval) for sharp_interval in sharp_intervals]
+# print(sharp_intervals[required_y])
+# print(merged_intervals[required_y])
+# print(sum(x[1] - x[0] + 1 for x in merged_intervals[required_y]))
+
